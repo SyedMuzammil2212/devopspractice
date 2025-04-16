5262aa13b5e980f1857ad1d1cce48528c0a211fb
SyedMuzammil2212 <syedsam2212@gmail.com>
2025-04-16 21:33:43 +0530

    antesports

---
+++ docback/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require("express")
+const cors = require("cors")
+
+
+const app = express()
+
+
+app.use(express.json())
+app.use(cors())
+
+const PORT = process.env.PORT || 5025
+
+app.get("/", async (req, res) => {
+    res.status(200).json({ message: "Hello mama" })
+})
+
+app.post("/hello", async (req, res) => {
+    const { name } = req.body
+
+    res.status(200).json({ message: `Your name is ${name} carzy` })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Running on ${PORT} `)
+})
