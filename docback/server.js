066217f53454e65528eb9292e2956a8ae76dc453
SyedMuzammil2212 <syedsam2212@gmail.com>
2025-04-17 00:02:52 +0530

    chuck

--- docback/server.js
+++ docback/server.js
@@ -12,7 +12,7 @@ app.use(cors())
 const PORT = process.env.PORT || 5025
 
 app.get("/", async (req, res) => {
-    res.status(200).json({ message: "Hello mama" })
+    res.status(200).json({ message: "Hello mama crazy" })
 })
 
 app.post("/hello", async (req, res) => {
